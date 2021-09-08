8e1f621f3e78e7c95a98af44b4855ef19a244fe3
anish-sridharan <anishsridharan26797@gmail.com>
2021-09-09 00:04:14 +0200

    Finished Code Q4 and Q5

--- Bayes.py
+++ Bayes.py
@@ -14,7 +14,6 @@ class Bayes:
         #prior = self.prior_list_[pos_hyp]
         pos_obs = self.observations_list_.index(observation)
         lk= self.likelihood_array_[pos_hyp][pos_obs]
-        lk = round(lk,3)
         return lk
     
     #Q3
@@ -22,21 +21,32 @@ class Bayes:
         norm_cst = 0
         for item in self.hypothesis_list_:
             prior = self.prior_list_[self.hypothesis_list_.index(item)]
-            norm_cst = norm_cst + prior*self.likelihood(observation)
-            norm_cst = round(norm_cst,3)
+            norm_cst = norm_cst + prior*self.likelihood(observation,item)
         return norm_cst    
 
     #Q4
     def single_posterior_update(self,observation,priors_list):
         posterior_list = []
+        count=0
         for prior in priors_list:
-            likelihood = self.likelihood(observation,self.hypothesis_list_[priors_list.index(prior)])
+
+            likelihood = self.likelihood(observation,self.hypothesis_list_[count])
             const = self.norm_constant(observation)
             posterior = (prior*likelihood)/const
             posterior = round(posterior,3)
             posterior_list.append(posterior)
+            count+=1
         return posterior_list    
-
+    
+    
+    #Q5
+    def compute_posterior(self,observations_list):
+        posterior_list = []
+        for observation in observations_list:
+            posterior=self.single_posterior_update(observation,self.prior_list_)
+            self.prior_list_=posterior
+            
+        return posterior
 
         
 
--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from Bayes import Bayes
 
 if __name__ == "__main__" :
+    #Problem 1
+    print("Answer to Problem1")
     hypos = ["Bowl1", "Bowl2"]
     priors = [0.5,0.5]
-    obs = ["chocolate", "vanilla"]
+    obs_list = ["chocolate", "vanilla"]
     likelihood = [[15/50 , 35/50], [30/50, 20/50]]
-    b = Bayes(hypos,priors,obs,likelihood)
+    b = Bayes(hypos,priors,obs_list,likelihood)
 
     l = b.likelihood("chocolate", "Bowl1")
-    round(l,)
+    round(l,3)
     print("likelihood(chococolate, Bowl1) =  " , l)
+
+    l = b.single_posterior_update("vanilla", [0.5, 0.5])
+   
+    print("single_posterior_updatedate(vanilla, [0.5, 0.5]) =  " , l)
+
+    l =b.compute_posterior(["chocolate", "vanilla"])
+   
+    print("chocolate, vannila =  " , l)
+
+
+    #Problem2
+    print("Answer to Problem 2")
+    hypos = ["Beginner", "Intermediate","Advanced","Expert"]
+    priors = [0.25,0.25,0.25,0.25]
+    obs_list = ["yellow", "red","blue","black","white"]
+    obs = ["yellow", "white","blue","red","red","blue"]
+    likelihood = [[0.05 , 0.1, 0.4, 0.25, 0.2], [0.1, 0.2, 0.4, 0.2, 0.1], [0.2, 0.4, 0.25, 0.1, 0.05],[0.3, 0.5, 0.125, 0.05, 0.025]]
+    b = Bayes(hypos,priors,obs_list,likelihood)
+
+   
+
+    l =b.compute_posterior(obs)
+   
+    print("yellow, white, blue, red, red, blue =  " , l)
